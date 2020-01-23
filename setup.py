f5a942870ff554afe964179ff5fa281d54f67494
Gregory Lee <grlee77@gmail.com>
2020-01-23 03:51:51 -0500

    add namespace_package argument to setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ cmdclass = {"test": PyTest}
 
 opts = dict(
     name=NAME,
+    namespace_package=["mrrt"],
     maintainer=MAINTAINER,
     maintainer_email=MAINTAINER_EMAIL,
     description=DESCRIPTION,
